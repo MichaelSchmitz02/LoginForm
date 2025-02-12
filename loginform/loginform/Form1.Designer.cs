26648b1c79f1945eb2fa4162fbab7fd72855ca04
MichaelSchmitz02 <152979941+MichaelSchmitz02@users.noreply.github.com>
2025-02-12 09:52:36 +0100

    Add files via upload

---
+++ loginform/loginform/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace loginform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtkrIme = new System.Windows.Forms.TextBox();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.btnPrijava = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtkrIme
+            // 
+            this.txtkrIme.Location = new System.Drawing.Point(227, 138);
+            this.txtkrIme.Name = "txtkrIme";
+            this.txtkrIme.Size = new System.Drawing.Size(162, 26);
+            this.txtkrIme.TabIndex = 0;
+            // 
+            // txtLozinka
+            // 
+            this.txtLozinka.Location = new System.Drawing.Point(227, 188);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.Size = new System.Drawing.Size(162, 26);
+            this.txtLozinka.TabIndex = 1;
+            // 
+            // btnPrijava
+            // 
+            this.btnPrijava.Location = new System.Drawing.Point(270, 246);
+            this.btnPrijava.Name = "btnPrijava";
+            this.btnPrijava.Size = new System.Drawing.Size(119, 39);
+            this.btnPrijava.TabIndex = 2;
+            this.btnPrijava.Text = "Prijava";
+            this.btnPrijava.UseVisualStyleBackColor = true;
+            this.btnPrijava.Click += new System.EventHandler(this.btnPrijava_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrijava);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.txtkrIme);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtkrIme;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.Button btnPrijava;
+    }
+}
+
